a74f4d24e6a7f6703e0760c67f956fdfc2aff59a
HQhalo <haoquangms@gmail.com>
2025-11-06 16:38:12 +0700

    autopad

--- nets/nn.py
+++ nets/nn.py
@@ -7,9 +7,9 @@ from utils.util import make_anchors
 
 def pad(k, p=None, d=1):
     if d > 1:
-        k = d * (k - 1) + 1
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k] 
     if p is None:
-        p = k // 2
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k] 
     return p
 
 
