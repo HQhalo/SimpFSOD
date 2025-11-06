852c54efe437443fb9e3fa9c899f94cd9910630d
HQhalo <haoquangms@gmail.com>
2025-11-06 16:32:19 +0700

    c2f

--- nets/nn.py
+++ nets/nn.py
@@ -71,6 +71,44 @@ class CSP(torch.nn.Module):
         y.extend(m(y[-1]) for m in self.res_m)
         return self.conv3(torch.cat(y, dim=1))
 
+class Bottleneck(torch.nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a standard bottleneck module with optional shortcut connection and configurable parameters."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """Applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+class C2f(torch.nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = torch.nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = self.cv1(x).split((self.c, self.c), 1)
+        y = [y[0], y[1]]
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
 
 class SPP(torch.nn.Module):
     def __init__(self, in_ch, out_ch, k=5):
@@ -91,13 +129,13 @@ class DarkNet(torch.nn.Module):
         super().__init__()
         p1 = [Conv(width[0], width[1], 3, 2)]
         p2 = [Conv(width[1], width[2], 3, 2),
-              CSP(width[2], width[2], depth[0])]
+              C2f(width[2], width[2], depth[0])]
         p3 = [Conv(width[2], width[3], 3, 2),
-              CSP(width[3], width[3], depth[1])]
+              C2f(width[3], width[3], depth[1])]
         p4 = [Conv(width[3], width[4], 3, 2),
-              CSP(width[4], width[4], depth[2])]
+              C2f(width[4], width[4], depth[2])]
         p5 = [Conv(width[4], width[5], 3, 2),
-              CSP(width[5], width[5], depth[0]),
+              C2f(width[5], width[5], depth[0]),
               SPP(width[5], width[5])]
 
         self.p1 = torch.nn.Sequential(*p1)
@@ -119,12 +157,12 @@ class DarkFPN(torch.nn.Module):
     def __init__(self, width, depth):
         super().__init__()
         self.up = torch.nn.Upsample(None, 2)
-        self.h1 = CSP(width[4] + width[5], width[4], depth[0], False)
-        self.h2 = CSP(width[3] + width[4], width[3], depth[0], False)
+        self.h1 = C2f(width[4] + width[5], width[4], depth[0], False)
+        self.h2 = C2f(width[3] + width[4], width[3], depth[0], False)
         self.h3 = Conv(width[3], width[3], 3, 2)
-        self.h4 = CSP(width[3] + width[4], width[4], depth[0], False)
+        self.h4 = C2f(width[3] + width[4], width[4], depth[0], False)
         self.h5 = Conv(width[4], width[4], 3, 2)
-        self.h6 = CSP(width[4] + width[5], width[5], depth[0], False)
+        self.h6 = C2f(width[4] + width[5], width[5], depth[0], False)
 
     def forward(self, x):
         p3, p4, p5 = x
